615f40a6791b237ac50fb5951a761204493e8b29
Git2coder <calculadoradepresupuesto@gmail.com>
2025-05-04 03:00:53 -0300

    borramos secrets

--- api/firebase.js
+++ api/firebase.js
@@ -1,18 +1,8 @@
 // api/firebase.js
 import admin from "firebase-admin";
-import { readFileSync } from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 
-// Para obtener __dirname en módulos ES
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-// Ruta relativa a la raíz del proyecto
-const serviceAccountPath = path.join(__dirname, "../secrets/serviceAccountKey.json");
-const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, "utf8"));
-
-// Inicializar solo una vez
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
